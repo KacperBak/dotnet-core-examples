803b3241931222158bf83dc9fc599d99796b6e7e
Kacper Bak <github@kacperbak.de>
2019-11-21 22:27:12 +0100

    Endpoint extended, VSCode Azure functions plugin used for debugging

--- HelloAzureFunctions/MyHttpTrigger.cs
+++ HelloAzureFunctions/MyHttpTrigger.cs
@@ -30,4 +30,26 @@ namespace HelloAzureFunctions
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
+
+    public static class MyHttpTrigger2
+    {
+        [FunctionName("MyHttpTrigger2")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+            string name = "Micha";
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
 }
